4ed2fe5278d6803b864e16b2c05d551a8fb8dd5f
Carvi3 <andrewcarvajal38@gmail.com>
2023-04-25 09:12:18 -0400

    CYKClass_Test.py -Unit Test Displays Structure by hardcoding the path (Without iterator) and displays the toString's of the nodes visited.
    CYKClass.py
    -Added First part of "CreateStructure"... It creates the skeleton.
    -Missing: Connection from branches. Found Conflict. May need to add another variable in the CykNode class called "adjacentParent" which will refer to the box, or node above another node.
    -Missing: Function to connect "adjacentParent" as an example. We cannot find the box of Z24 as there is no straight path leading to the floor, Z22. Meaning Cartesian Product and Union will not be accessed yet.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import CYKClass as cyk
 import CYKClass_Test as cykTest
-
+def HelloWorld():
+    print("Hello CYK!")
 def main():
+    #Just to ensure Interpreter Works
+    HelloWorld()
+
     #init will run and start test class
     testClass = cykTest.CYKClass_Test()
     print("Main Terminated")
